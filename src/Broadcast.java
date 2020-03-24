34c2e3ac72f068d2157592985ac006308c34173f
Lichtschalter-5000 <spitzbubenkoenig@web.de>
2020-03-24 23:43:36 +0100

    Code hübsch gemacht und so

--- src/Broadcast.java
+++ src/Broadcast.java
@@ -1,50 +1,43 @@
 import ea.*;
 
 public class Broadcast
-extends Server
-{
-    
-    
-    
-    public Broadcast(int port) 
-    {
+        extends Server {
+
+
+    public Broadcast(int port) {
         super(port);
     }
-    
-    
- 
-   
+
+
     @Override
-    public void empfangeString (String string) 
-    {
+    public void empfangeString(String string) {
         sendeString(string);
     }
- 
-    
+
+
     @Override
-    public void verbindungBeendet () 
-    {
+    public void verbindungBeendet() {
         sendeString("Info an alle: Ein Client hat gerade die Verbindung abgebrochen.");
     }
 
     @Override
-    public void empfangeInt (int i) {
+    public void empfangeInt(int i) {
     }
- 
+
     @Override
-    public void empfangeByte (byte b) {
+    public void empfangeByte(byte b) {
     }
- 
+
     @Override
-    public void empfangeDouble (double d) {
+    public void empfangeDouble(double d) {
     }
- 
+
     @Override
-    public void empfangeChar (char c) {
+    public void empfangeChar(char c) {
     }
- 
+
     @Override
-    public void empfangeBoolean (boolean b) {
+    public void empfangeBoolean(boolean b) {
     }
 }
 
